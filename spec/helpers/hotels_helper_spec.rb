394dfbf4493ef66808c39d68aa23800b910ffce5
Jen-Mei Wu <jenmei@colorxp.com>
2011-02-23 14:56:36 -0800

    Unit 4 walkthrough 3 finished.

--- spec/helpers/hotels_helper_spec.rb
+++ spec/helpers/hotels_helper_spec.rb
@@ -1,15 +1,9 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the HotelsHelper. For example:
-#
-# describe HotelsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe HotelsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "date formater" do
+    it "should return the current day in English" do
+      day_today().should eq(Date.today.strftime("%A"))
+    end
+  end
 end
