2427e937aebe8aae28ea7e4ab5d5466896e963e6
Jen-Mei Wu <jenmei@colorxp.com>
2011-02-24 10:20:34 -0800

    Temp commit to try stuff out on windows

---
+++ spec/integration/hotels_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "hotels" do
+  describe "listing hotels" do
+    before do
+      @hotel1 = Hotel.create(
+        :name => "The Lakes"
+      )
+      @hotel2 = Hotel.create(
+        :name => "The St. Clair"
+      )
+      visit "/hotels"
+    end
+
+    it "should list as many hotels as we have" do
+      page.all('ul li').should have(2).hotels
+    end
+
+    it "should list all hotel names (xpath)" do
+      page.should have_xpath( "//li[.='#{@hotel1.name}']")
+      page.should have_xpath("//li[.='#{@hotel2.name}']")
+    end
+
+
+    it "should list all hotel names (css)" do
+      hotel_names = page.all('ul li').map(&:text)
+      hotel_names.should include(@hotel1.name)
+      hotel_names.should include(@hotel2.name)
+    end
+  end
+end
