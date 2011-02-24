8253c67a26e4f14316d063156263c3e9d067978a
Jen-Mei Wu <jenmei@colorxp.com>
2011-02-24 11:13:40 -0800

    unit_5_walkthrough_2

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,9 @@
 class HotelsController < ApplicationController
+  def index
+    @hotels = Hotel.order('name')
+  end
+
+  def show
+    @hotel = Hotel.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Traveljournal::Application.routes.draw do
   resources :hotels
   resources :trips
+
+  root :to => "hotels#index"
+  match ":id/hotels" => "hotels#show", :via => :get, :as => :pretty_url
 end
